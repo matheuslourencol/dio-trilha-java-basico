d4f7977ed8947d8115d4e76015f814f1de8fac0f
matheuslourencol <matheuslourencoml10@gmail.com>
2024-11-09 14:40:00 -0300

    conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World final");
+        
+        int numeroConta;
+        String agencia, nomeCliente;
+        double saldo;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta! ");
+        numeroConta = teclado.nextInt();
+        System.out.println("Porf favor, difite a Agência! ");
+        agencia = teclado.next();
+        System.out.println("Por favor, digite seu Nome! ");
+        nomeCliente = teclado.next();
+        System.out.println("Por favor, digite o valor do primeiro deposito! ");
+        saldo = teclado.nextDouble();
+        
+        
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+        + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já esta disponível para saque");
+
+
+
+
     }
 }
